bdbb255b549debe5165d77f3b763adac9df5ca71
pihugupt-a <gpihu@tisb.ac.in>
2025-01-14 21:21:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-hi guys
-st.title("🎈 My new app")
+
+st.title("TISB HACKS")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
