85ca4d4dbe8858916907af4db0476ea246de071e
saanvikhetan <saanvikhetan@gmail.com>
2025-01-14 21:23:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit as st
 
-st.title("TISB yay")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("")
