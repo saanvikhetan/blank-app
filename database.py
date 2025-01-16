b19a16d7529433eb22636b9ef2ee576f7e82235f
saanvikhetan <saanvikhetan@gmail.com>
2025-01-16 18:40:30 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-conn2 = st.connection("gsheets2", type=GSheetsConnection)
+#conn2 = st.connection("gsheets2", type=GSheetsConnection)
 
 df = conn.read()
-df2 = conn2.read()
+#df2 = conn2.read()
 
 
 st.write(df)
-st.write(df2)
+#st.write(df2)
