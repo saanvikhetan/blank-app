973da3edce021050a6aacfe075977d6593861640
saanvikhetan <saanvikhetan@gmail.com>
2025-01-16 18:58:43 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
