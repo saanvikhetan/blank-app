0ffab5c8dbc2d3bf777d92109a6523bdd5718e8b
pihugupt-a <gpihu@tisb.ac.in>
2025-01-17 17:27:24 +0530

    Update streamlit_app_4.py

--- streamlit_app_4.py
+++ streamlit_app_4.py
@@ -98,7 +98,7 @@ if "eco_points" in st.session_state and st.session_state.quiz_completed:
         st.sidebar.write(f"**{progress_level['title']}**")
         st.sidebar.write(progress_level["description"])
         st.sidebar.write(f"Points: {st.session_state.eco_points}")
-        st.sidebar.write(f"**Total Annual Carbon Footprint:** {st.session_state.total_emissions:.2f} tons of CO₂e")
+        st.sidebar.write(f"**Total Carbon Footprint:** {st.session_state.total_emissions:.2f} tons of CO₂e")
 
 # --- Quiz Section ---
 if not st.session_state.quiz_completed:
